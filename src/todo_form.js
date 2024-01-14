e72b3a21d0dd5b2cebcb6d284d9c0fbdada8de86
Thinh Phan <pseudothinhphan@gmail.com>
2024-01-14 15:51:06 -0800

    Add duedate to the loop to reduce the code amount

--- src/todo_form.js
+++ src/todo_form.js
@@ -13,21 +13,24 @@ export default function createTodo() {
         for: "name",
         type: "text",
         id: "name",
-        autocomplete: "off",
         required: "",
       },
       {
         for: "details",
         type: "text",
         id: "details",
-        autocomplete: "off",
         required: "",
       },
       {
         for: "priority",
         type: "text",
         id: "priority",
-        autocomplete: "off",
+        required: "",
+      },
+      {
+        for: "duedate",
+        type: "date",
+        id: "duedate",
         required: "",
       },
     ];
@@ -43,7 +46,6 @@ export default function createTodo() {
         input.setAttributes({
             type: todoSimilarProperties[i]["type"],
             id: todoSimilarProperties[i]["id"],
-            autocomplete: todoSimilarProperties[i]["autocomplete"],
             required: todoSimilarProperties[i]["required"],
         });
 
@@ -51,27 +53,13 @@ export default function createTodo() {
         ul.appendChild(li);
     }
 
-    const duedateLi = document.createElement("li");
-    const duedateTitle = document.createElement("label");
-    duedateTitle.textContent = "Due Date";
-    duedateTitle.setAttribute("for", "duedate");
-    const duedate = document.createElement("input");
-    duedate.setAttributes({
-      type: "date",
-      id: "duedate",
-      autocomplete: "off",
-      required: "",
-    });
-    duedateLi.append(duedateTitle, duedate);
-
     const submitLi = document.createElement("li");
     const submitBtn = document.createElement("button");
     submitBtn.textContent = "Submit";
     submitBtn.setAttribute("type", "submit");
     submitLi.appendChild(submitBtn);
 
-    ul.append(duedateLi, submitLi);
+    ul.append(submitLi);
     form.appendChild(ul);
     body.appendChild(form);
-
 }
