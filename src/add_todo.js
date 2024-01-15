084d50df8d9fa065060bc66d1b92063551e06157
Thinh Phan <pseudothinhphan@gmail.com>
2024-01-14 16:26:57 -0800

    Create a separate JS file for addTodo

---
+++ src/add_todo.js
@@ -0,0 +1,29 @@
+import { Todo } from './create_todo';
+
+export default function addTodo(event) {
+  event.preventDefault();
+
+  const mainContainer = document.getElementById("container");
+  let name = document.querySelector("#name").value;
+  let details = document.querySelector("#details").value;
+  let priority = document.querySelector("#priority").value;
+  let duedate = document.querySelector("#duedate").value;
+
+  let newTodo = new Todo(name, details, duedate, priority);
+
+  const todoElement = document.createElement("div");
+  const todoName = document.createElement("div");
+  todoName.textContent = newTodo.name;
+  const todoDetails = document.createElement("div");
+  todoDetails.textContent = newTodo.details;
+  const todoDueDate = document.createElement("div");
+  todoDueDate.textContent = newTodo.duedate;
+  const todoPriority = document.createElement("div");
+  todoPriority.textContent = newTodo.priority;
+
+  todoElement.append(todoName, todoDetails, todoDueDate, todoPriority);
+
+  mainContainer.appendChild(todoElement);
+
+  this.reset();
+}
