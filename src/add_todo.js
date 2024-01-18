f2c1a4e1248ba7f7d58ae305dc365c0afc919ce3
Thinh Phan <pseudothinhphan@gmail.com>
2024-01-17 17:29:13 -0800

    Add id for each todo and class for each property

--- src/add_todo.js
+++ src/add_todo.js
@@ -2,6 +2,8 @@ import { Todo } from './create_todo';
 import deleteTodo from './delete_todo';
 import finishTodo from './finish_todo';
 
+let idCounter = 1;
+
 export default function addTodo(event) {
   event.preventDefault();
 
@@ -10,20 +12,25 @@ export default function addTodo(event) {
   let details = document.querySelector("#details").value;
   let duedate = document.querySelector("#duedate").value;
   let priority = document.querySelector("#priority").value;
-
   let newTodo = new Todo(name, details, duedate, priority);
 
   const todoElement = document.createElement("div");
+  todoElement.setAttribute('id', idCounter);
+  idCounter++;
   const todoCheckbox = document.createElement("input");
   todoCheckbox.setAttribute("type", "checkbox");
   const todoName = document.createElement("div");
   todoName.textContent = newTodo.name;
+  todoName.setAttribute('class', 'name');
   const todoDetails = document.createElement("div");
   todoDetails.textContent = newTodo.details;
+  todoDetails.setAttribute('class', 'details');
   const todoDueDate = document.createElement("div");
   todoDueDate.textContent = newTodo.duedate;
+  todoDueDate.setAttribute('class', 'duedate');
   const todoPriority = document.createElement("div");
   todoPriority.textContent = newTodo.priority;
+  todoPriority.setAttribute('class', 'priority');
   const deleteTodoBtn = document.createElement("button");
   deleteTodoBtn.textContent = "X";
    
