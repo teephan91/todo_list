5359720496d03cc655454543c949f0a6a19d61ae
Thinh Phan <pseudothinhphan@gmail.com>
2024-02-04 18:27:19 -0800

    Remove the outside div from todo form btn

--- src/todo_form_btn.js
+++ src/todo_form_btn.js
@@ -1,9 +1,7 @@
 export default function createTodoFormBtn(btn_value) {
-    const div = document.createElement('div');
     const btn = document.createElement('button');
     btn.textContent = btn_value;
     btn.setAttribute("type", btn_value);
-    div.appendChild(btn);
 
-    return div;
+    return btn;
 }
