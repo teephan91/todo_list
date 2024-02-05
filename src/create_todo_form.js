bbbc376acc1a55f9b8dc876f83eb69e77e033c8a
Thinh Phan <pseudothinhphan@gmail.com>
2024-02-04 18:23:43 -0800

    Clean up the add todo form

--- src/create_todo_form.js
+++ src/create_todo_form.js
@@ -1,4 +1,3 @@
-import createProjectName from "./todo_form_project";
 import createSimilarProperties from "./todo_form_similar_props";
 import createPriorityProperty from "./todo_form_priority";
 import createTodoFormBtn from "./todo_form_btn";
@@ -9,20 +8,16 @@ Element.prototype.setAttributes = function (obj) {
     }
 };
 
-export default function createTodoForm(form_class, project_value, name_value, details_value, duedate_value, priority_value, btn_value) {
+export default function createTodoForm(form_class, name_value, details_value, duedate_value, priority_value, btn_value) {
     const form = document.createElement('form');
-    const div = document.createElement('div');
 
     form.setAttribute('class', form_class);
 
-    div.appendChild(createProjectName(project_value));
-    div.appendChild(
+    form.appendChild(
       createSimilarProperties(name_value, details_value, duedate_value)
     );
-    div.appendChild(createPriorityProperty(priority_value));
-    div.appendChild(createTodoFormBtn(btn_value));
-
-    form.appendChild(div);
+    form.appendChild(createPriorityProperty(priority_value));
+    form.appendChild(createTodoFormBtn(btn_value));
     
     return form;
 }
--- src/todo_form_project.js
+++
@@ -1,6 +0,0 @@
-export default function createProjectName(project_value) {
-    const div = document.createElement('div');
-    div.textContent = project_value;
-
-    return div;
-}
