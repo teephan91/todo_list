38abfdf49a34fc65bd2c1b41a014c7870aa671cc
Thinh Phan <pseudothinhphan@gmail.com>
2024-02-11 17:13:31 -0800

    Add close button to todo + edit form

--- src/todo_form_btn.js
+++ src/todo_form_btn.js
@@ -1,7 +1,21 @@
 export default function createTodoFormBtn(btn_value) {
-    const btn = document.createElement('button');
-    btn.textContent = btn_value;
-    btn.setAttribute("type", btn_value);
+    const div = document.createElement('div');
+    const submitBtn = document.createElement('button');
+    const closeBtn = document.createElement('button');
+    
+    submitBtn.textContent = btn_value;
+    submitBtn.setAttribute('type', btn_value);
 
-    return btn;
+    closeBtn.textContent = 'X';
+    closeBtn.setAttribute('type', 'button');
+    closeBtn.style.backgroundColor = 'maroon';
+    closeBtn.addEventListener('click', removeAddTodoForm);
+
+    div.append(submitBtn, closeBtn);
+
+    return div;
+}
+
+function removeAddTodoForm() {
+    this.parentNode.parentNode.remove();
 }
