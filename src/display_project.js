d6281d717f98fc41e049c6a95898df272c8063aa
Thinh Phan <pseudothinhphan@gmail.com>
2024-02-11 17:14:13 -0800

    Clean code

--- src/display_project.js
+++ src/display_project.js
@@ -1,7 +1,6 @@
 export default function displayProject() {
     const todoContainer = document.querySelector('.todo_container');
     const projectDisplaying = document.getElementById(this.parentNode.className);
-    console.log(projectDisplaying);
 
     for (const project of todoContainer.children) {
         project.style.display = 'none';
